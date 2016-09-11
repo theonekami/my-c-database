ff3ee1c8dd83015b51de603ece2c5be7df180fd0
theonekami <theonekamisama@gmail.com>
2016-09-11 16:56:37 +0530

    Update map.h

--- map.h
+++ map.h
@@ -1,227 +1,107 @@
+#ifndef MAP_H_INCLUDED
+#define MAP_H_INCLUDED
+
 #include "kev.h"
-class janken
+
+class genmap
 {
-char name[50];
-public:
-int id;
-janken();
-janken(char a[],int);
-void display();
-}stone("Stone",1),
-paper("Paper",2),
-scissor("Scissor",3);
-janken::janken(char a[],int n)
-{id=n;
-strcpy(name,a);
-}
-janken::janken()
-{int a;
-}
-void janken::display()
-{cout<<name<<endl<<endl;
-}
-void check(janken a,janken b)
-{clrscr();
-delay(1000);
-cout<<"stone"<<endl;
-delay(1000);
-cout<<"paper"<<endl;
-delay(1000);
-cout<<"scissor"<<endl;
-delay(1000);
-cout<<"GO!!"<<endl;
-a.display();
-b.display();
-if(a.id==b.id)
-{cout<<"Draw |]";
-return;
-}
- else if(a.id==1)
- {if(b.id==2)
+  protected:
+  int sizeX,sizeY,posY,posX;
+  char map[50][50];
+  char BX,BY,spwn;
+  public:
+    genmap(int x,int y,char X='_',char Y='|');
+    void disp();
+    void map_spwn(char a='~',int i=-1,int j=-1);
+    int move_down();
+    void map_rem(char a='~');
+
+};
+
+genmap::genmap(int x,int y,char X,char Y)
   {
-  cout<<"LOSE :(";
-  return;
-  }
-  else
-  {
-  cout<<"WIN!! :)";
-  return;
-  }
- }
- else if(a.id==2)
- {if(b.id==3)
-  {
-  cout<<"LOSE :(";
-  return;
-  }
-  else
-  {
-  cout<<"WIN!! :)";
-  return;
-  }
- }
- else if(a.id==3)
- {if(b.id==1)
-  {
-  cout<<"LOSE :(";
-  return;
-  }
-  else
-  {
-  cout<<"WIN!! :)";
-  return;
-  }
- }
-}
-void main()
-{clrscr();
-int ch;
-char c;
-janken p,a;
-do
-{clrscr();
-randomize();
-cout<<"choose\n\
-1)STONE!!!\n\
-2)PAPER___\n\
-3)SCISSORS XYXYXY(press no)\n";
-cin>>ch;
-switch(ch)
+    sizeX=x;
+    sizeY=y*2;
+    BX=X;
+    BY=Y;
+    for (int i=0;i<sizeX;i++)
+       {
+	 if(i==0||i==sizeX-1)
+	   {
+	    for (int j=0;j<sizeY;j++)
+		{
+		 map[i][j]=X;
+	}
+	    }
+
+	 else
+	   {
+	     for (int j=0;j<sizeY;j++)
+		 map[i][j]=' ';
+		 map[i][sizeY-1]=Y;
+		 map[i][0]=Y;
+	   }
+
+	}
+    }
+
+void genmap::disp()
+	{
+	  for(int k=0;k<sizeX;k++)
+	      {
+	       for (int j=0;j<sizeY;j++)
+	       cout<<map[k][j];
+	       cout<<endl;
+	      }
+	}
+
+void genmap::map_spwn(char a,int i,int j)
+       {
+       if(i<0&&j<0)
+	{
+	do
+	{
+	 randomize();
+	 i=rand()%sizeX;
+	 j=rand()%sizeY;
+      /*cout<<map[i][j];
+	 getch();  errorr detection */
+	}while(map[i][j]==BX||map[i][j]==BY);
+	}
+	else if(map[i][j]==BX||map[i][j]==BY)
+	{
+	cout<<"map not generated cause you disrupted";
+	getch();
+	exit(0);
+	}
+	map[i][j]=a;
+	spwn=a;
+	posX=i;
+	posY=j;
+	}
+
+int genmap::move_down()
 {
-case 1:p=stone;
-       break;
-case 2:p=paper;
-       break;
-case 3:p=scissor;
-       break;
+int temp;
+temp=posX;
+posX++;
+//cout<<posX<<temp;
+//getch(); errorr detection
+if(map[posX][posY]!=' ')
+{return -1;
 }
-ch=random(3)+1;
-switch(ch)
-{
-case 1:a=stone;
-       break;
-case 2:a=paper;
-       break;
-case 3:a=scissor;
-       break;
+map[posX][posY]=map[temp][posY];
+map[temp][posY]=' ';
+return 0;
 }
-check(p,a);
-getch();
-cout<<"again?(y/n)";
-cin>>c;
-}while (c=='y');
-getch();
-}#include "kev.h"
-class janken
-{
-char name[50];
-public:
-int id;
-janken();
-janken(char a[],int);
-void display();
-}stone("Stone",1),
-paper("Paper",2),
-scissor("Scissor",3);
-janken::janken(char a[],int n)
-{id=n;
-strcpy(name,a);
-}
-janken::janken()
-{int a;
-}
-void janken::display()
-{cout<<name<<endl<<endl;
-}
-void check(janken a,janken b)
-{clrscr();
-delay(1000);
-cout<<"stone"<<endl;
-delay(1000);
-cout<<"paper"<<endl;
-delay(1000);
-cout<<"scissor"<<endl;
-delay(1000);
-cout<<"GO!!"<<endl;
-a.display();
-b.display();
-if(a.id==b.id)
-{cout<<"Draw |]";
-return;
-}
- else if(a.id==1)
- {if(b.id==2)
-  {
-  cout<<"LOSE :(";
-  return;
-  }
-  else
-  {
-  cout<<"WIN!! :)";
-  return;
-  }
- }
- else if(a.id==2)
- {if(b.id==3)
-  {
-  cout<<"LOSE :(";
-  return;
-  }
-  else
-  {
-  cout<<"WIN!! :)";
-  return;
-  }
- }
- else if(a.id==3)
- {if(b.id==1)
-  {
-  cout<<"LOSE :(";
-  return;
-  }
-  else
-  {
-  cout<<"WIN!! :)";
-  return;
-  }
- }
-}
-void main()
-{clrscr();
-int ch;
-char c;
-janken p,a;
-do
-{clrscr();
-randomize();
-cout<<"choose\n\
-1)STONE!!!\n\
-2)PAPER___\n\
-3)SCISSORS XYXYXY(press no)\n";
-cin>>ch;
-switch(ch)
-{
-case 1:p=stone;
-       break;
-case 2:p=paper;
-       break;
-case 3:p=scissor;
-       break;
-}
-ch=random(3)+1;
-switch(ch)
-{
-case 1:a=stone;
-       break;
-case 2:a=paper;
-       break;
-case 3:a=scissor;
-       break;
-}
-check(p,a);
-getch();
-cout<<"again?(y/n)";
-cin>>c;
-}while (c=='y');
-getch();
-}
+
+/*void genmap::map_rem(char a)
+       {
+       for (int i=0;i<sizeX;i++)
+       {for (int j=0;j<sizeY;j++)
+	if(map[i][j]==a)
+	{map[i][j]==
+	}
+       }
+       }      */
+#endif
